8703f7e3e1d39ab279e70a03d052e11c04963816
tnvmadhav <tnvmadhav@gmail.com>
2023-07-09 14:14:03 +0530

    Message: not backwards compatible?

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,12 +1,19 @@
-import Head from 'next/head';
-import Layout from '../../components/Layout';
-import { getAllPostIds, getPostData } from '../../lib/posts';
-import Date from '../../components/date';
-import utilStyles from '../../styles/utils.module.css';
-import { GetStaticPaths, GetStaticProps } from 'next';
+import Layout from '../../components/Layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import { GetStaticProps, GetStaticPaths } from 'next'
 
-
-export default function Post({ postData }) {
+export default function Post({
+  postData
+}: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
   return (
     <Layout>
       <Head>
@@ -15,29 +22,27 @@ export default function Post({ postData }) {
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
-          <Date dateString={postData.date}/>
+          <Date dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
-  );
+  )
 }
-
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const postData = await getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
-}
-
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = getAllPostIds();
-    return {
-      paths,
-      fallback: true,
-    };
-};
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params?.id as string)
+  return {
+    props: {
+      postData
+    }
+  }
+}
