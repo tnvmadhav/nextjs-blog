d5092af99bcdbe0bc857f960bec607148eb3966c
tnvmadhav <tnvmadhav@gmail.com>
2023-07-09 13:38:30 +0530

    Message: NextJS Tutorial from the documentation

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {hostname: 'tnvmadhav.me'}
+        ]
+    }
+  }
+   
+module.exports = nextConfig
